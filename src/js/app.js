66045b0395595fb588e97cee1d08018bcae2f8e7
Axel Lanuza <rojasf@ae.com>
2024-11-21 16:02:08 -0600

    3. add docs folder for deploying on github pages

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,14 @@ import { Tabs } from "./test-module";
 import "../css/app.css";
 import "../scss/style.scss";
 
+const $tabContainer = document.querySelector(".tabs");
 /********** Paste your code here! ************/
 
-window.onload = () => {
-  console.log("Paste your code here!");
-  Tabs.init();
-};
+// window.onload = () => {
+//   console.log("Paste your code here!");
+//   Tabs.init($tabContainer);
+// };
+
+document.addEventListener("DOMContentLoaded", () => {
+  Tabs.init($tabContainer);
+});
--- src/js/test-module.js
+++ src/js/test-module.js
@@ -4,13 +4,45 @@ export const Tabs = (() => {
    *   DOM refs
    *
    * */
+  let $tabContainer;
+  let $tabsNavElements;
+  let $tabsContentElements;
 
-  const init = () => {
-    console.log("Init app");
-    eventHandlers();
+  let currentTabIndex = 0;
+
+  const init = (tabContainer) => {
+    console.log("Init Tabs app");
+    console.log(`outtertabcontainer: ${tabContainer}`);
+    $tabContainer = tabContainer;
+    $tabsNavElements = $tabContainer.querySelectorAll(".tabs-list-element");
+    $tabsContentElements = $tabContainer.querySelectorAll(".tab-content");
+    if ($tabsContentElements) eventHandlers();
   };
 
-  const eventHandlers = () => {};
+  const eventHandlers = () => {
+    $tabsNavElements.forEach((element, index) => {
+      element.dataset.index = index;
+      element.addEventListener("click", (e) => {
+        const liElment = e.target;
+        currentTabIndex = liElment.dataset.index;
+        $tabsNavElements.forEach((element) => {
+          element.classList.remove("active");
+        });
+        liElment.classList.add("active");
+        showTabContent(currentTabIndex); // Show the content for the clicked tab
+      });
+    });
+  };
+
+  const showTabContent = (index) => {
+    $tabsContentElements.forEach((content, contentIndex) => {
+      if (contentIndex == index) {
+        content.classList.remove("hidden");
+      } else {
+        content.classList.add("hidden");
+      }
+    });
+  };
 
   return {
     init,
